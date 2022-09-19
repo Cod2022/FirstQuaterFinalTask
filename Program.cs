babb24d0a2a902dd6c3b1ad7edde43329eb4b21a
My Name <Me@gmail.com>
2022-09-20 00:08:15 +0400

    1 commit wrote PrintArray

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+string[] array = {"Den", "Samarkand", "Nickname", "23"};
+
+void PrintArray (string[] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write(array[i]);
+        Console.WriteLine();
+    }
+}
+
+
+PrintArray(array);
